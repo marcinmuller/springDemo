1d75b90cd6c1bc35b65ee61bd067282cf63609df
prog15V <mmuller@o2.pl>
2018-06-09 17:50:49 +0200

    2

--- src/main/java/pl/mmuller/spring/demo/DemoApplication.java
+++ src/main/java/pl/mmuller/spring/demo/DemoApplication.java
@@ -2,11 +2,24 @@ package pl.mmuller.spring.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RestController
+    @RequestMapping("/api")
+    public class ApiController{
+
+        @GetMapping("/greeting")
+        public String getGreeting(){
+            return "Hello world";
+        }
     }
 }
